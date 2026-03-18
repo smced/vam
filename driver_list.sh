84d12cc013050b26c7cf32069cb5acadb492a038
smced <144388983+smced@users.noreply.github.com>
2026-03-18 10:27:59 -0600

    Update driver_list.sh

--- driver_list.sh
+++ driver_list.sh
@@ -1,5 +1,5 @@
 sudo docker exec -i $(sudo docker ps --filter "name=AssetManager_postgres" --format "{{.ID}}") \
-  psql -U 'Ap1D4t4b4s3%4dm1nUs3rn4m3' -d vam \
+  psql -U 'Ap1D4t4b4s3%4dm1nUs3rn4m3' -d vam --csv \
   -c "
 WITH asset_props AS (
   SELECT \"AssetId\", \"PropertyName\", \"Value\"::text AS \"Value\"
@@ -7,13 +7,15 @@ WITH asset_props AS (
   WHERE \"PropertyName\" IN (
     'ADI - Configuration - Driver Name',
     'ADI - Network - Hostname',
-    'Network - MAC Address Vendor'
+    'Network - MAC Address Vendor',
+    'Network - TCP Listening Ports'
   )
 )
 SELECT
   string_agg(CASE WHEN \"PropertyName\" = 'ADI - Configuration - Driver Name' THEN \"Value\" END, ', ') AS \"DriverName\",
   string_agg(CASE WHEN \"PropertyName\" = 'ADI - Network - Hostname'          THEN \"Value\" END, ', ') AS \"Hostname\",
-  string_agg(CASE WHEN \"PropertyName\" = 'Network - MAC Address Vendor'      THEN \"Value\" END, ', ') AS \"MACAddressVendor\"
+  string_agg(CASE WHEN \"PropertyName\" = 'Network - MAC Address Vendor'      THEN \"Value\" END, ', ') AS \"MACAddressVendor\",
+  string_agg(CASE WHEN \"PropertyName\" = 'Network - TCP Listening Ports'     THEN \"Value\" END, ', ') AS \"TCPListeningPorts\"
 FROM asset_props
 GROUP BY \"AssetId\"
 ORDER BY \"AssetId\"
