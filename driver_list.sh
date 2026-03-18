5fd3bb0c742e86c8437b922dcbb12f4c88867230
smced <144388983+smced@users.noreply.github.com>
2026-03-18 10:14:46 -0600

    Create driver_list.sh

---
+++ driver_list.sh
@@ -0,0 +1,20 @@
+sudo docker exec -i $(sudo docker ps --filter "name=AssetManager_postgres" --format "{{.ID}}") \
+  psql -U 'Ap1D4t4b4s3%4dm1nUs3rn4m3' -d vam \
+  -c "
+WITH asset_props AS (
+  SELECT \"AssetId\", \"PropertyName\", \"Value\"::text AS \"Value\"
+  FROM \"PropertyValue\"
+  WHERE \"PropertyName\" IN (
+    'ADI - Configuration - Driver Name',
+    'ADI - Network - Hostname',
+    'Network - MAC Address Vendor'
+  )
+)
+SELECT
+  string_agg(CASE WHEN \"PropertyName\" = 'ADI - Configuration - Driver Name' THEN \"Value\" END, ', ') AS \"DriverName\",
+  string_agg(CASE WHEN \"PropertyName\" = 'ADI - Network - Hostname'          THEN \"Value\" END, ', ') AS \"Hostname\",
+  string_agg(CASE WHEN \"PropertyName\" = 'Network - MAC Address Vendor'      THEN \"Value\" END, ', ') AS \"MACAddressVendor\"
+FROM asset_props
+GROUP BY \"AssetId\"
+ORDER BY \"AssetId\"
+" | tee ~/driver_list.csv
